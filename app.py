bc449a2249b67c6a88f781e28a559d03b9f81b73
IMINABO ROBERTS <iminaboroberts@gmail.com>
2024-12-14 18:47:09 -0500

    change port

--- app.py
+++ app.py
@@ -36,5 +36,9 @@ def submit_record():
 
     # return jsonify({'alert_message': the_response})
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    # port = int(os.environ.get("PORT", 8000))  # Use the PORT environment variable or default to 8000
+    app.run(host="0.0.0.0", port=8000, debug=True)
